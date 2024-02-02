2637e9999b0224f4abff9e5cfb625f2fd30af724
TheArmega <sanmasja@gmail.com>
2024-02-02 19:44:55 +0100

    [Repository Restructure] Root folder as ROS2 Motor GUI Workspace[Repository Restructure] Root folder as ROS2 TEO Workspace [Fix] Corrected Binaries with Wrong References
    In this commit, I have addressed and fixed the issue where certain binaries had incorrect references. The problem was causing the binaries to reference the wrong dependencies or resources, resulting in unexpected behavior.
    
    I have updated the binaries to point to the correct references, ensuring that they work as intended. This fix enhances the reliability and functionality of the affected components.
    
    Please review and merge this commit to incorporate the necessary corrections.

--- build/py_motor_gui/prefix_override/sitecustomize.py
+++ build/py_motor_gui/prefix_override/sitecustomize.py
@@ -1,3 +1,3 @@
 import sys
 sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/teo/ros2-teo-ws/install/py_motor_gui'
+sys.prefix = sys.exec_prefix = '/home/teo/ros2-motor-gui/install/py_motor_gui'
--- install/setup.sh
+++ install/setup.sh
@@ -7,7 +7,7 @@
 # since a plain shell script can't determine its own path when being sourced
 # either use the provided COLCON_CURRENT_PREFIX
 # or fall back to the build time prefix (if it exists)
-_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/home/teo/ros2-teo-ws/install
+_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/home/teo/ros2-motor-gui/install
 if [ ! -z "$COLCON_CURRENT_PREFIX" ]; then
   _colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX="$COLCON_CURRENT_PREFIX"
 elif [ ! -d "$_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX" ]; then
